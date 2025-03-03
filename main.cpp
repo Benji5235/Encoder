2f617d82fcaa3cbad57f2da4c3d0bbc1cd3821aa
Benji5235 <Benji12122013@gmail.com>
2025-03-03 08:48:27 -0500

    This is for RCS Team Designation

--- main.cpp
+++ main.cpp
@@ -2,108 +2,13 @@
 #include <FEHIO.h>
 #include <FEHUtility.h>
 #include <FEHMotor.h>
-
-//Declarations for encoders & motors
-//This is a change required for reasons also Evan lol
-DigitalEncoder right_encoder(FEHIO::P1_0);
-DigitalEncoder left_encoder(FEHIO::P2_0);
-DigitalEncoder forward_encoder(FEHIO::P0_1);
-FEHMotor right_motor(FEHMotor::Motor0,9.0);
-FEHMotor left_motor(FEHMotor::Motor1,9.0);
-FEHMotor forward_motor(FEHMotor::Motor2,9.0);
-
-
-void move_forward(int percent, int counts) //using encoders
-{
-    //Reset encoder counts
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
-
-    //Set both motors to desired percent
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(-(percent));
-
-    //While the average of the left and right encoder is less than counts,
-    //keep running motors
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
-
-    //Turn off motors
-    right_motor.Stop();
-    left_motor.Stop();
-}
-
-void turn_left()
-{
-    //Reset encoder counts
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
-    int counts = 255;
-
-    //Set both motors to desired percent
-    left_motor.Stop();
-    right_motor.SetPercent(30);
-
-    //While the average of the left and right encoder is less than counts,
-    //keep running motors
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
-
-    //Turn off motors
-    right_motor.Stop();
-    left_motor.Stop();
-}
-
-void turn_right()
-{
-    //Reset encoder counts
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
-    int counts = 255;
-
-    //Set both motors to desired percent
-    left_motor.SetPercent(30);
-    right_motor.Stop();
-
-    //While the average of the left and right encoder is less than counts,
-    //keep running motors
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
-
-    //Turn off motors
-    right_motor.Stop();
-    left_motor.Stop();
-}
+#include <FEHRCS.h>
 
 int main(void)
 {
-    int motor_percent = 30; //Input power level here
-    int expected_counts = 567; //Input theoretical counts here
-
-    float x, y; //for touch screen
-
-    //Initialize the screen
-    LCD.Clear(BLACK);
-    LCD.SetFontColor(WHITE);
-
-    LCD.WriteLine("Shaft Encoder Exploration Test");
-    LCD.WriteLine("Touch the screen");
-    while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
-    while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
-
-    move_forward(motor_percent, 1200); //see function
-    Sleep(10.0);
-    move_forward(motor_percent, 1200);
-    Sleep(1.0);
-    move_forward(-motor_percent, 1200);
-
-    Sleep(2.0); //Wait for counts to stabilize
-    //Print out data
-    LCD.Write("Theoretical Counts: ");
-    LCD.WriteLine(expected_counts);
-    LCD.Write("Motor Percent: ");
-    LCD.WriteLine(motor_percent);
-    LCD.Write("Actual LE Counts: ");
-    LCD.WriteLine(left_encoder.Counts());
-    LCD.Write("Actual RE Counts: ");
-    LCD.WriteLine(right_encoder.Counts());
-
-    return 0;
+ RCS.InitializeTouchMenu("0800A9VHR"); // Run Menu to select Region (e.g., A, B, C, D)
+ int lever = RCS.GetLever(); // Get a 0, 1, or 2 indicating which lever to pull
+ LCD.WriteLine( RCS.Time() ); // Get the match time remaining
+//  Sleep(2.0);
+//  LCD.WriteLine(lever);
 }
