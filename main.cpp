f187e9593c2b8f72bec0718803ab4096d634852a
Benji5235 <Benji12122013@gmail.com>
2025-03-03 21:04:35 -0500

    Exploration 3 Part 3: Position Correcting Using a Distance Sensor

--- main.cpp
+++ main.cpp
@@ -4,99 +4,42 @@
 #include <FEHMotor.h>
 #include <FEHRCS.h>
 
+#define pi 3.14159
+
 FEHMotor left_motor(FEHMotor::Motor2, 9.0);
 FEHMotor right_motor(FEHMotor::Motor1, 9.0);
 DigitalInputPin distanceSensor(FEHIO::P3_0);
-int turn_dir = 0;
-bool var = true;
+DigitalEncoder right_encoder(FEHIO::P0_0);
+DigitalEncoder left_encoder(FEHIO::P1_0);
+
+
+void move_forward(int percent, int counts){
+
+}
+
 int main(void) 
 { 
-    int objectInRange = distanceSensor.Value();
-    LCD.WriteLine(objectInRange);
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
 
-    //Go straight
-    while (objectInRange == 1){
-        objectInRange = distanceSensor.Value();
-        LCD.WriteLine(objectInRange);
-        right_motor.SetPercent(-25);
-        left_motor.SetPercent(-25);
+    //One inch (theoretically)
+    int one_inch = (318/(2*pi*1.25));
+    while ((right_encoder.Counts() + left_encoder.Counts())/2<one_inch*24){
+        left_motor.SetPercent(30);
+        right_motor.SetPercent(30);
     }
 
-    //Back up a little bit
-    left_motor.SetPercent(20);
-    right_motor.SetPercent(20);
-    Sleep(1.0);
+    //Stops the motor for 3 seconds
+    left_motor.Stop();
+    right_motor.Stop();
+    Sleep(3.0);
 
-    //Turn left
-    objectInRange = distanceSensor.Value();
-    LCD.WriteLine(objectInRange);
-    right_motor.SetPercent(-20);
-    left_motor.SetPercent(20);
-    Sleep(1.65);
-
-    //Go straight
-    while (objectInRange == 1){
-        objectInRange = distanceSensor.Value();
-        LCD.WriteLine(objectInRange);
-        right_motor.SetPercent(-25);
-        left_motor.SetPercent(-25);
+    //Move back until 5cm away
+    while (distanceSensor.Value()!=1){
+        left_motor.SetPercent(-15);
+        right_motor.SetPercent(-15);
     }
 
-    //Back up a little bit
-    left_motor.SetPercent(20);
-    right_motor.SetPercent(20);
-    Sleep(1.0);
-
-    //Turn right
-    objectInRange = distanceSensor.Value();
-    LCD.WriteLine(objectInRange);
-    right_motor.SetPercent(20);
-    left_motor.SetPercent(-20);
-    Sleep(1.75);
-    
-
-    //Go straight
-    while (objectInRange == 1){
-        objectInRange = distanceSensor.Value();
-        LCD.WriteLine(objectInRange);
-        right_motor.SetPercent(-25);
-        left_motor.SetPercent(-25);
-    }
-
-    //End
-    LCD.WriteLine(objectInRange);
     right_motor.Stop();
     left_motor.Stop();
-
-    // while(var)
-    // {
-    //     //Go straight
-    //     while (objectInRange > 0.5){
-    //         right_motor.SetPercent(30);
-    //         left_motor.SetPercent(30);
-    //     }
-
-    //     //Turn right
-    //     if (objectInRange < 0.5){
-    //         if (turn_dir == 0)
-    //         {
-    //             right_motor.SetPercent(-20);
-    //             left_motor.SetPercent(20);
-    //             Sleep(1.0);
-    //             left_motor.Stop();
-    //             right_motor.Stop();
-    //             turn_dir = 1;
-    //         }
-    //         if (turn_dir == 1)
-    //         {
-    //             right_motor.SetPercent(20);
-    //             left_motor.SetPercent(-20);
-    //             Sleep(1.0);
-    //             left_motor.Stop();
-    //             right_motor.Stop();
-    //             turn_dir = 0;
-    //         }
-    //     }
-            
-    // }
 } 
